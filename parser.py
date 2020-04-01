09e2114dee3afe843300eb604767a28211e05b67
Egor <egor_mixajlichenko@mail.ru>
2020-04-02 01:00:47 +0300

    add second version

--- parser.py
+++ parser.py
@@ -1,3 +1,16 @@
-import requests
-from bs4 import BeautifulSoup
+from tkinter import *
 
+window = Tk()
+window.geometry ('300x300')
+
+def get_coord(event):
+	get_coord_x = event.x
+	get_coord_y = event.y
+	print(f'x: {get_coord_x}  y: {get_coord_y}')
+
+
+
+
+
+window.bind('<Button-1>', get_coord)
+window.mainloop()
