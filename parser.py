71e4ed305f270dbd7fd88f2e1f552d0a215f0bcf
Egor <egor_mixajlichenko@mail.ru>
2020-04-02 20:20:56 +0300

    объектный рефактoринг'

--- parser.py
+++ parser.py
@@ -1,16 +1,52 @@
 from tkinter import *
+from random import randint
 
-window = Tk()
-window.geometry ('300x300')
+WIDTH = 500
+HEIGHT = 500
+
+
 
 def get_coord(event):
-	get_coord_x = event.x
-	get_coord_y = event.y
-	print(f'x: {get_coord_x}  y: {get_coord_y}')
+	print(f'x: {event.x}  y: {event.y}')
 
 
+class Ball:
+	def __init__(self):
+		self.R =randint(20,50)
+		self.x = randint(self.R, WIDTH - self.R)
+		self.y = randint(self.R, HEIGHT - self.R)
+		self.dx, self.dy = (+10,+10)
+		self.ball_id = canvas.create_oval(self.x - self.R, self.y - self.R, self.x + self.R, self.y + self.R , fill = 'green')
+
+	def move(self):
+		self.x += self.dx
+		self.y += self.dy
+		if self.x + self.R > WIDTH or self.x - self.R <= 0:
+			self.dx = -self.dx
+		if self.y + self.R > HEIGHT or self.y - self.R <= 0:
+			self.dy = -self.dy
+
+	def show(self):
+		canvas.move(self.ball_id, self.dx, self.dy)
 
 
+def tick():
+	ball.move()
+	ball.show()
+	window.after(50, tick)
 
-window.bind('<Button-1>', get_coord)
-window.mainloop()
+
+def main():
+	global window, canvas, ball
+
+	window = Tk()
+	window.geometry(str(WIDTH) + "x" + str(HEIGHT))
+	canvas = Canvas(window, width = WIDTH, height = HEIGHT, bg = 'black')
+	canvas.pack(anchor = "nw", fill = BOTH)
+	canvas.bind('<Button-1>', get_coord)
+	ball = Ball()
+	tick()
+	window.mainloop()
+
+if __name__ == '__main__':
+	main()
